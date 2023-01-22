44b09d54219d81977b285018fb73cc6ab34d1a1e
rian000614 <iyanirawan71@gmail.com>
2023-01-22 20:09:46 +0700

    Finishing Feature Login and Register

--- app/src/main/java/com/example/aplikasigamesuit/MainActivity.kt
+++ app/src/main/java/com/example/aplikasigamesuit/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.aplikasigamesuit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aplikasigamesuit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var loginPref: LoginPref
+
+    companion object {
+        const val KEY_NAME = "key_name"
+        const val KEY_NAME_FROM_DIALOG = "key_name_from_dialog"
+        const val KEY_NAME_FROM_MAIN = "key_name_from_main"
+        const val TAG = "MenuActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loginPref = LoginPref(this)
+
+        }
     }
-}
