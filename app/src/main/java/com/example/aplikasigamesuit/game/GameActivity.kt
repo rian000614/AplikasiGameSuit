ee1cb82860f3f08403b0a5e137e36a8aabe7d7a8
arieffahmi <arieffahminuur@gmail.com>
2023-01-23 16:33:13 +0700

    commit

--- app/src/main/java/com/example/aplikasigamesuit/game/GameActivity.kt
+++ app/src/main/java/com/example/aplikasigamesuit/game/GameActivity.kt
@@ -1,4 +1,4 @@
-package com.example.aplikasigamesuit
+package com.example.aplikasigamesuit.game
 
 import android.content.Context
 import android.os.Bundle
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.aplikasigamesuit.R
 import com.example.aplikasigamesuit.utils.Constants
 import com.example.aplikasigamesuit.utils.Constants.KEY_NAME
 import com.example.aplikasigamesuit.utils.Constants.KEY_PLAYER_TWO
